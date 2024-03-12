f2864892254be814254e08ff3af0558a30457097
Md Nasim Hosen <mdnasimhosen333@gmail.com>
2024-03-12 23:11:13 +0600

    learn batch and transition

---
+++ src/batch.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const batchTransaction = async () => {
+  const createUser = prisma.user.create({
+    data: {
+      username: "jhankar",
+      email: "jkr@ph.com",
+    },
+  });
+
+  const updateUser = prisma.user.update({
+    where: {
+      id: 1,
+    },
+    data: {
+      age: 30,
+    },
+  });
+
+  const [userData, updateData] = await prisma.$transaction([
+    createUser,
+    updateUser,
+  ]);
+
+  console.log(userData, updateData);
+};
+
+batchTransaction();
